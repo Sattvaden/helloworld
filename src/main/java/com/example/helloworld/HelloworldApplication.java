5cce104118d7f6d9699da962c6b361319b472378
Denis Saenko <dsaenko@yandex.ru>
2020-11-27 22:40:27 +0100

    first  commit

---
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+
+    @RestController
+    class HelloworldController {
+        @GetMapping("/{name}")
+        String hello(@PathVariable String name) {
+            return "Hello " + name + "!";
+        }
+    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
